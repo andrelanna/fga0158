1d0b156a12d69ab91c75b7eb388cd1c4e26eaf38
dieg0d <diegodi05@hotmail.com>
2021-09-08 15:09:08 -0300

    retangulo e circulo implementados

---
+++ aula3/exemplo/figurasGeometricas/Retangulo.java
@@ -0,0 +1,28 @@
+package figurasGeometricas;
+
+public class Retangulo {
+
+	private float altura;
+    private float largura;
+
+	public Retangulo(float alturaRetangulo, float larguraRetangulo) {
+		this.altura = alturaRetangulo;
+		this.largura = larguraRetangulo;
+	}
+
+	public float calcularPerimetro() {
+		return (altura * 2) + (largura * 2); 
+	}
+	
+	public float calcularArea() {
+		return altura * largura; 
+	}
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public float getLargura() {
+		return largura;
+	}
+}
