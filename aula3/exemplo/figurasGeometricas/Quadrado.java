fcc6f9032e9211358027f58d7614fd5dfd3e9c5f
davimatheusgp <55845596+davimatheusgp@users.noreply.github.com>
2021-09-08 16:02:53 -0300

    Add files via upload

--- aula3/exemplo/figurasGeometricas/Quadrado.java
+++ aula3/exemplo/figurasGeometricas/Quadrado.java
@@ -1,22 +1,22 @@
 package figurasGeometricas;
 
 public class Quadrado {
-
-	private float lado;
-
-	public Quadrado(float ladoQuadrado) {
-		this.lado = ladoQuadrado;
-	}
-
-	public float calcularPerimetro() {
-		return 4 * lado; 
-	}
+			
+		private float lado;
 	
-	public float calcularArea() {
-		return lado * lado; 
-	}
-
-	public float getLado() {
-		return lado;
-	}
+		public Quadrado(float ladoQuadrado) {
+			this.lado = ladoQuadrado;
+		}
+	
+		public float calcularPerimetro() {
+			return 4 * lado; 
+		}
+		
+		public float calcularArea() {
+			return lado * lado; 
+		}
+	
+		public float getLado() {
+			return lado;
+		}
 }
