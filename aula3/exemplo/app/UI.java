71214306a3baa99d4a451dbeded9e4519fb59d03
RRLTrinity <90349025+RRLTrinity@users.noreply.github.com>
2021-09-08 16:00:21 -0300

    Aula 3 - Rodrigo Ribeiro Lopes Trindade 17/0113922
    Atualizando UI

--- aula3/exemplo/app/UI.java
+++ aula3/exemplo/app/UI.java
@@ -3,20 +3,21 @@ package app;
 import javax.swing.JOptionPane;
 
 import figurasGeometricas.Quadrado;
+import figurasGeometricas.Circulo;
 
 public class UI {
 	
 	static Quadrado[] quadrados = new Quadrado[0];  //referencia representa a associacao MULTIPLA entre UI e Quadrado
-
+	static Circulo[] circulos = new Circulo[0];  //referencia representa a associacao MULTIPLA entre UI e Circulo
 	
 	
 	public static void main(String[] args) {
-		//Aqui Ã© onde tudo comeÃ§a! 
+		//Aqui é onde tudo começa! 
 		
 		int opcao;
 		do { 
 			//1a coisa: definir qual figura vai ser criada
-			String strOpcao = JOptionPane.showInputDialog("Escolha uma opÃ§Ã£o de figura geomÃ©trica: \n"
+			String strOpcao = JOptionPane.showInputDialog("Escolha uma opção de figura geométrica: \n"
 					+ "1 - Quadrado\n"
 					+ "2 - Retangulo\n"
 					+ "3 - Circulo\n"
@@ -49,7 +50,23 @@ public class UI {
 
 			case 3: 
 				//criar circulo
+				String strRaioCirculo = JOptionPane.showInputDialog("Informe o raio do circulo:");
+				float raioCirculo = Float.parseFloat(strRaioCirculo);
+				Circulo c = new Circulo(raioCirculo);
+
+				//inserir c dentro do vetor circulos.
+				//Esforco bracal! 
+				Circulo[] temp3 = new Circulo[circulos.length + 1];
+				for (int i=0; i<circulos.length; i++) {
+					temp3[i] = circulos[i];  //copiando todas referencias de circulos em temp3
+				}
+				//inserir c em temp3
+				temp3[circulos.length] = c;
+				//atualizar referencia de circulos
+				circulos = temp3;
+
 				break;
+				
 
 			case 0: 
 				//sair do programa
@@ -73,24 +90,17 @@ public class UI {
 			JOptionPane.showMessageDialog(null, resposta);
 		}
 
+		for (int i=0; i<circulos.length; i++) {
+			String resposta = "Circulo " + i + "(" + circulos[i] + ")" + ":\n"
+					+ "Raio: " + circulos[i].getLado() + "\n"
+					+ "Area: " + circulos[i].calcularArea() + "\n"
+					+ "Perimetro: " + circulos[i].calcularPerimetro();
+
+			JOptionPane.showMessageDialog(null, resposta);
+		}
 
 			//Fazer com as demais figuras geometricas
 
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
