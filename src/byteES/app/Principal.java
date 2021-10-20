ba9a1f212e8c3ad75d13a21b5f237c1579dc0cc3
Andre Lanna <andre.lanna@gmail.com>
2021-10-20 15:02:58 -0300

    E/S byte a byte

---
+++ src/byteES/app/Principal.java
@@ -0,0 +1,13 @@
+package byteES.app;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Cadastro c = new Cadastro();
+		c.lerAlunos();
+		c.cadastrarAluno();
+		c.gravarAlunos();
+	}
+
+}
