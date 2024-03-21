9790e7cd2b8264398ee59facac03387b0174bb8e
JaoComTil <124062209+JaoComTil@users.noreply.github.com>
2024-03-20 22:21:33 -0300

    Create Exercicio3.java

---
+++ aula2/fga0158/Exercicio3.java
@@ -0,0 +1,28 @@
+
+package fga0158;
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+public static void main(String[] args) {
+    //Declaracao de variaveis
+    *** declaracao do scanner
+    *** declaracao da variavel a 
+    *** declaracao da variavel b
+    *** declaraces de uma variavel para cada operação 
+
+    //inicializacao do scanner
+    **scanner** = new **_____(___)**;
+
+    //leitura dos valores no teclado: substituas as variaveis e complete o método
+    **variavel a**  = **scanner**.next___();
+    **variavel b**  = **scanner**.next___();
+    
+    //cálculo das operações
+    int resultado = a + b;
+    System.out.println("O resultado da soma é " + ***);
+     .
+     .
+     .
+}
+}
