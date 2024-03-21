4b1c02c79833e382364266140457ec1a031429e1
JaoComTil <124062209+JaoComTil@users.noreply.github.com>
2024-03-20 22:07:46 -0300

    Create Exercicio1-Aula2.java

---
+++ aula2/fga0158/Exercicio1-Aula2.java
@@ -0,0 +1,18 @@
+Obs.: o trecho de código
+abaixo apresenta a estrutura básica para a resolução desse exercício. Altere os
+trechos destacados com as instruções em Java para resolução do exercício. 
+
+
+public static void main(String[] args) {
+
+    //Declaracao de variáveis
+    Scanner **variavel** = new Scanner(System.in);
+    String **variavel** = **qual comando colocar aqui?**;
+    
+    //saída em modo texto
+    System.out.println("Olá " + **qual variavel colocar aqui?** + ", Seja bem vindo!");
+    
+    //Saída em modo gráfico
+    String **variavel** = JOptionPane.showInputDialog("Qual o seu nome?: ");
+    JOptionPane.showMessageDialog(null, "Olá " + **qual variavel colocar aqui?** + ", Seja bem vindo!");
+}
